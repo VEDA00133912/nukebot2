7fa04f537c74ad3046525cbd4904fa0c502f6fd4
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-29 17:20:58 +0900

    Add files via upload

--- modules/commands/ping.js
+++ modules/commands/ping.js
@@ -1,1 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('ping'),
+  async execute(interaction) {
+
+    const embed = new EmbedBuilder()
+      .setColor('ff0000')
+      .setTitle('ping :ping_pong:')
+      .setDescription('Ping値')
+      .setTimestamp()
+      .setFooter({ text:'BOT | Ping'})
+      .addFields(
+        { name: 'WebSocket Ping', value: `${interaction.client.ws.ping}ms`, inline: true },
+        { name: 'API-Endpoint Ping', value: `${Date.now() - interaction.createdTimestamp}ms`, inline: true },
+        );
+
+    try {
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.log("ping error");
+      await interaction.reply('エラーが発生しました')
+    }
+  },
+};
