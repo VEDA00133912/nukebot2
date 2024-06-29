1aafc395f1a235dc3c9fb4150841fdf4854d959b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-29 17:21:36 +0900

    Add files via upload

--- modules/events/interaction.js
+++ modules/events/interaction.js
@@ -1,1 +1,20 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
 
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`${interaction.commandName} がありません`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'エラーが発生しました', ephemeral: true });
+        }
+    },
+};
